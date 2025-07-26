fb9c7ab2aef034858b2d64287fa284a2b219ab99
rabahmusayid <rabahmusayid@gmail.com>
2025-07-26 18:09:06 +0300

    Fixed refund and terms sections + config updates

--- src/components/sections/privacy.tsx
+++ src/components/sections/privacy.tsx
@@ -8,7 +8,9 @@ export default function PrivacyPolicy() {
 
             <h2>1. Overview</h2>
             <p>
-                We are committed to protecting your privacy. This Privacy Policy outlines how your personal information is collected, used, and protected when you purchase our digital products such as Shopify Liquid code snippets.
+                Market Lightning LLC is  committed to protecting your privacy. This Privacy Policy outlines how your personal information is collected, used, and protected when you purchase our digital products such as Shopify Liquid code snippets.
+
+
             </p>
 
             <h2>2. What We Collect</h2>
