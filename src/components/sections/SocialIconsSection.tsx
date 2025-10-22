d8538a1f6544e9dbc34efe7b87d35dd1273f2471
MOHAMMED120sa <rabah734116@gmail.com>
2025-10-22 11:39:25 +0300

    Update SocialIconsSection.tsx

--- src/components/sections/SocialIconsSection.tsx
+++ src/components/sections/SocialIconsSection.tsx
@@ -8,12 +8,12 @@ import {
 const icons = [
     {
         icon: <FaWhatsapp />,
-        href: 'https://wa.me/966509324662',
+        href: 'https://wa.me/967774739149',
         bg: 'bg-green-500'
     },
     {
         icon: <FaTelegramPlane />,
-        href: 'https://web.telegram.org/k/#@Toptoall',
+        href: 'https://web.telegram.org/k/#8137200776',
         bg: 'bg-blue-400'
     },
     {
@@ -23,12 +23,12 @@ const icons = [
     },
     {
         icon: <FaInstagram />,
-        href: 'https://www.instagram.com/futer7788?igsh=ZGUzMzM3NWJiOQ==',
+        href: 'https://www.instagram.com/marketlightning?igsh=MWU1bmUwdmhzeXFqMw==',
         bg: 'bg-gradient-to-tr from-pink-500 via-red-500 to-yellow-500'
     },
     {
         icon: <FaFacebookF />,
-        href: 'https://www.facebook.com/profile.php?id=61576701425912',
+        href: 'https://www.facebook.com/share/1NhJa2NYMS/',
         bg: 'bg-blue-700'
     }
 ];
